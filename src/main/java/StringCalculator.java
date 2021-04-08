ba23fed2d0b5565f577a3ddfe827e110d0683629
Joacim <joacim.bondas@ha.ax>
2021-04-08 13:33:07 +0300

    Allowing the Add method to handle new lines between numbers (instead of commas) and checking for illegal input as stated in the text "the following input is NOT ok: “1,\n”".

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,19 @@
 import java.util.Arrays;
 
 public class StringCalculator {
-    public static int add(String numbers) {
-        return Arrays.stream(numbers.split(","))
-                .filter(number->!"".equals(number))
+
+
+    public static int add(String numbers) throws NumberFormatException {
+        for (int i = 0; i < numbers.length(); i++) {
+            for (int j = i + 1; j < numbers.length(); j++) {
+                if (!Character.isDigit(numbers.charAt(i)) && !Character.isDigit(numbers.charAt(j))) {
+                    double d = Double.parseDouble(numbers);
+                }
+            }
+        }
+
+        return Arrays.stream(numbers.split("\\s+|,\\s*|\\.\\s*"))
+                .filter(number -> !"".equals(number))
                 .mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/java/StringCalulatorTest.java
+++ src/test/java/StringCalulatorTest.java
@@ -1,21 +1,38 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StringCalulatorTest {
+
+
     @Test
     public void add_givenEmptyString_expectZero() {
-        Assertions.assertEquals(0,StringCalculator.add(""));
-
+        assertEquals(0,StringCalculator.add(""));
     }
 
     @Test
     public void add_givenOne_expectOne() {
-        Assertions.assertEquals(1,StringCalculator.add("1"));
-
+        assertEquals(1,StringCalculator.add("1"));
     }
     @Test
-    public void add_givenOnePlusTwo_expectOne() {
-        Assertions.assertEquals(3,StringCalculator.add("1,2"));
+    public void add_givenOnePlusTwo_expectThree() {
+        assertEquals(3,StringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void add_givenSeveralNumbers_expectEight() {
+        assertEquals(8,StringCalculator.add("1,2,3,2"));
+    }
+
+    @Test
+    public void add_givenOnePlusTwoPlusThreeWithNewline_expectSix() {
+        assertEquals(6,StringCalculator.add("1\n2,3"));
+    }
+    @Test
+    public void add_givenOneCommaNewline_expectException() {
+        assertThrows(NumberFormatException.class, ()->StringCalculator.add("1,\n"));
 
     }
+
 }
