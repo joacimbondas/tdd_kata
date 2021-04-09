5fb268a6aae68d860c2192aba8df2349676020c2
Joacim <joacim.bondas@ha.ax>
2021-04-09 10:19:48 +0300

    Test for 7=VII

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -10,26 +10,28 @@ public class RomanNumerals {
         String[] romanNumbers = {"I", "V", "X", "L", "C", "D", "M"};
         List<Integer> arabicNumbersList = new ArrayList<>(Arrays.asList(arabicNumbers));
         List<String> romanNumbersList = new ArrayList<>(Arrays.asList(romanNumbers));
-        int[] allowedMultipleIndex = {0,2,4,6};
-        if(number<4 && number > 0){
+        int[] allowedMultipleIndex = {0, 2, 4, 6};
+        if (number < 4 && number > 0) {
             for (int i = 0; i < number; i++) {
                 roman = roman + "I";
             }
         }
 
-        for (int i = 1; i < arabicNumbers.length; i++) {
+        for (int i = 1; i < arabicNumbers.length - 1; i++) {
             if (arabicNumbers[i] == number) {
                 return romanNumbers[i];
-            }
-            else if(arabicNumbers[i]-arabicNumbers[i-1]==number){
-                return romanNumbers[i-1]+romanNumbers[i];
-            }
-            else if(arabicNumbers[i]+arabicNumbers[i-1]==number){
-                return romanNumbers[i]+romanNumbers[i-1];
+            } else if (arabicNumbers[i] - arabicNumbers[i - 1] == number) {
+                return romanNumbers[i - 1] + romanNumbers[i];
+            } else if (arabicNumbers[i] + arabicNumbers[i - 1] == number) {
+                return romanNumbers[i] + romanNumbers[i - 1];
+            } else if (number < arabicNumbers[i + 1] && number > arabicNumbers[i]) {
+                int tempNumber = number - arabicNumbers[i];
+                roman = romanNumbers[i];
+                for (int j = 0; j < tempNumber; j++) {
+                    roman = roman + "I";
+                }
             }
         }
-
-
 
 
         return roman;
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -8,20 +8,29 @@ public class RomanNumeralsTest {
     public void convert_givenTwo_expectII() {
         assertEquals("II", RomanNumerals.convert(2));
     }
+
     @Test
     public void convert_givenThree_expectIII() {
         assertEquals("III", RomanNumerals.convert(3));
     }
+
     @Test
     public void convert_givenFive_expectV() {
         assertEquals("V", RomanNumerals.convert(5));
     }
+
     @Test
     public void convert_givenFour_expectIV() {
         assertEquals("IV", RomanNumerals.convert(4));
     }
+
     @Test
     public void convert_givenSix_expectVI() {
         assertEquals("VI", RomanNumerals.convert(6));
+    }
+
+    @Test
+    public void convert_givenSeven_expectVII() {
+        assertEquals("VII", RomanNumerals.convert(7));
     }
 }
